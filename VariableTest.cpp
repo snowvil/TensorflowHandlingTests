96e51a03392192e621915f6ed09e0afd5074c4d7
haben <bene.hartmann@web.de>
2018-01-27 16:04:09 +0100

    First learning. Yeaaah

--- VariableTest.cpp
+++ VariableTest.cpp
@@ -70,3 +70,55 @@ TEST(Variable, VariableFloatAdd)
 	st = session.Run({ { { input,{ 2 } } } }, { add }, &output);
 	EXPECT_EQ(3.2F, *output.at(0).scalar<float>().data());
 }
+
+TEST(Variable, ManipulateVariable)
+{
+	int x = 1;
+	int y = 5;
+	float weight = 0.0;
+
+	TensorflowWrapperInit;
+	ops::Placeholder y_expected = ops::Placeholder(root.WithOpName("y_expected"), DT_INT32);
+
+	ops::Variable var = ops::Variable(root, { 1 }, DT_FLOAT);
+	Output varAssign = ops::Assign(root, var, { weight });
+
+	ClientSession session(root);
+	Status st{ session.Run({ varAssign }, &output) };
+	EXPECT_EQ(weight, *output.at(0).scalar<float>().data());
+	output.clear();
+
+	// adding the values. This is the basic model, which
+	Output add = ops::Add(root, var, ops::Cast(root, input, DT_FLOAT));
+
+	// calculating the difference between calculated value (output from 'add') and
+	// the expected value 'y_expected'
+	Output diff = ops::Sub(root, ops::Cast(root, y_expected, DT_FLOAT), add);
+
+	/*st = session.Run({ { { input,{ x } },{ y_expected,{y} } } }, { diff }, &output);
+	float oDiff = *(output.at(0).scalar<float>().data());
+	EXPECT_EQ(1.0, oDiff);
+	output.clear();*/
+
+	// multiplicate the obtained difference with a learning rate (0.1F)
+	ops::Mul mul = ops::Mul(root, diff, { 0.1F });
+	// add the learning value to the variable and assign the new value to the variable
+	Output updateVariable = ops::Add(root, var, mul);
+	Output varAssignNew = ops::Assign(root, var, updateVariable);
+
+	// calculate the value 1000 times
+	for (size_t i = 0; i < 1000; i++)
+	{
+		st = session.Run({ { { input,{ x } },{ y_expected,{ y } } } }, { varAssignNew }, &output);
+		// just to see the change of the variable
+		if ((i < 20))
+		{
+			::std::cout<< i << "\t:" << *(output.at(0).scalar<float>().data()) << "\n";
+		}
+	}
+
+	// see if the real model gets the desired value
+	st = session.Run({ { { input,{ x } } } }, { add }, &output);
+	float oModelOutput = *(output.at(0).scalar<float>().data());
+	EXPECT_FLOAT_EQ(2.0, oModelOutput);
+}
