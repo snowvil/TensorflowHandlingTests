415073ae6c0ccda9d6d69fcdecd3b78a8adbd2b5
haben <bene.hartmann@web.de>
2018-02-07 22:46:11 +0100

    moving the session creation into the helper macro!

--- testhelper.h
+++ testhelper.h
@@ -1,4 +1,5 @@
 #define TensorflowWrapperInit \
 	Scope root = Scope::NewRootScope(); \
 	ops::Placeholder input = ops::Placeholder(root.WithOpName("input"), DT_INT32); \
-	::std::vector<Tensor> output;
+	::std::vector<Tensor> output;\
+	ClientSession session(root);
